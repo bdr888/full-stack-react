77530950384fdd7c8b893a45a9163154d16e0cbd
briandridge <briandridge@gmail.com>
2018-08-20 23:03:32 -0600

    feat(server): cookies

--- server/index.js
+++ server/index.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User'); //this needs to be before passport
 require('./services/passport');
 
-mongoose.connect(encodeURI(keys.mongoURI), {
-  auth: {
-    user: keys.mongoUser,
-    password: keys.mongoPassword,
-  }
-});
+mongoose.connect(
+  encodeURI(keys.mongoURI),
+  {
+    auth: {
+      user: keys.mongoUser,
+      password: keys.mongoPassword,
+    },
+  },
+);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
